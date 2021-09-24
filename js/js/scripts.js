509fe9eeed59e42b49a624fb46d80239791af683
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-09-24 17:36:08 -0500

    Add js files

---
+++ js/js/scripts.js
@@ -0,0 +1,13 @@
+const names = ['Jahir', 'Pablo', 'Laura', 'Camila'];
+// forEach
+// map
+// filter
+// some
+// every
+
+const numbers = [ 2, 4 , 5, 7, 8, 9]
+const every = numbers.every((item) => {
+  return item > 0
+})
+
+console.log(every);
