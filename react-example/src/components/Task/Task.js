1254479ccd7e953d62ad69703589fccb69641522
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-10-09 10:39:18 -0500

    Allow deleting tasks

--- react-example/src/components/Task/Task.js
+++ react-example/src/components/Task/Task.js
@@ -1,7 +1,13 @@
 import PropTypes from 'prop-types';
+import { httpDelete } from '../../utils/fetch';
 import './Task.css';
 
 const Task = (props) => {
+  const deleteTask = async () => {
+    const resultado = await httpDelete(`${process.env.REACT_APP_BACKEND_URL}/tasks/${props.id}`);
+    window.location.reload(false);
+  };
+
   return (
     <div className='task'>
       <input type={'checkbox'} checked={props.done} />
@@ -9,9 +15,9 @@ const Task = (props) => {
         <h6>{props.task}</h6>
         <p>{`Due: ${props.due}`}</p>
 
-        <a href={`/edit/${props.id}`}>
-          Editar tarea
-        </a>
+        <a href={`/edit/${props.id}`}>Editar tarea</a>
+
+        <button onClick={deleteTask}>Eliminar tarea</button>
       </div>
     </div>
   );
@@ -21,6 +27,6 @@ Task.propTypes = {
   task: PropTypes.string.isRequired,
   due: PropTypes.object.isRequired,
   done: PropTypes.bool, // boolean
-}
+};
 
 export default Task;
