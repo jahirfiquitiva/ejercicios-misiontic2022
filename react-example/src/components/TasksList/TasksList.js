b16b781d825ecc5c39b989ba2b1381866e1700e4
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-10-20 18:36:38 -0500

    Implement search for tasks

--- react-example/src/components/TasksList/TasksList.js
+++ react-example/src/components/TasksList/TasksList.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { httpGet } from '../../utils/fetch';
 import { getToken } from '../../utils/getToken';
@@ -6,6 +6,18 @@ import Task from './../Task/Task'; // Task.js
 
 const TasksList = (props) => {
   const [tasks, setTasks] = useState([]);
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useMemo(() => {
+    if (!filter) setFilteredTasks(tasks);
+
+    setFilteredTasks(
+      tasks.filter((task) => {
+        return task.task.toLowerCase().includes(filter.toLowerCase());
+      })
+    );
+  }, [filter, tasks]);
 
   useEffect(() => {
     const token = getToken();
@@ -17,18 +29,35 @@ const TasksList = (props) => {
     const getTasks = async () => {
       const tasks = await httpGet(`${process.env.REACT_APP_BACKEND_URL}/tasks`);
       setTasks(tasks);
+      setFilteredTasks(tasks);
     };
     getTasks();
   }, []);
 
   return (
-    <div>
-      <Link to={'/login'}>Crear tareas</Link>
+    <>
+      <div>
+        <input
+          type='text'
+          value={filter}
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
+        />
+      </div>
       <br />
-      {(tasks || []).map((item, index) => {
-        return <Task key={index} id={item._id} task={item.task} due={item.due} done={item.done} />;
-      })}
-    </div>
+      <br />
+      <div>
+        <Link to={'/login'}>Crear tareas</Link>
+        <br />
+        <br />
+        {(filteredTasks || []).map((item, index) => {
+          return (
+            <Task key={index} id={item._id} task={item.task} due={item.due} done={item.done} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
